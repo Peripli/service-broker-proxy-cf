be56ff30c7ced263c9e6a37b68ec95c92fb35d7b
Kiril Kabakchiev <kiril.kabakchiev@sap.com>
2018-07-02 17:44:36 +0300

    resolve conflicts

--- main.go
+++ main.go
@@ -9,31 +9,23 @@ import (
 	"github.com/Peripli/service-broker-proxy/pkg/middleware"
 	"github.com/Peripli/service-broker-proxy/pkg/sbproxy"
 	"github.com/sirupsen/logrus"
-<<<<<<< Temporary merge branch 1
-	"github.com/Peripli/service-broker-proxy-cf/cf"
-	"github.com/Peripli/service-broker-proxy/pkg/env"
 	"github.com/cloudfoundry-community/go-cfenv"
-)
-
-func main() {
-	cfApp, err := cfenv.Current()
-	if err != nil {
-		logrus.WithError(err).Fatal("Error loading CF VCAP environment")
-=======
 )
 
 func main() {
 	var cfEnv sb.Environment
 	if _, isCFEnv := os.LookupEnv("VCAP_APPLICATION"); isCFEnv {
-		cfEnv = cf.NewCFEnv(env.Default(""))
+		cfApp, err := cfenv.Current()
+		if err != nil {
+			logrus.WithError(err).Fatal("Error loading CF VCAP environment")
+		}
+		cfEnv = cf.NewCFEnv(env.Default(""),cfApp)
 	} else {
 		cfEnv = sb.Default("")
 	}
 	if err := cfEnv.Load(); err != nil {
 		logrus.WithError(err).Fatal("Error loading environment")
->>>>>>> Temporary merge branch 2
 	}
-	cfEnv := cf.NewCFEnv(env.Default(""), cfApp)
 
 	platformConfig, err := cf.NewConfig(cfEnv)
 	if err != nil {

fbe82bcf4bf319aa1049637fcb1424961caa5458
Kiril Kabakchiev <kiril.kabakchiev@sap.com>
2018-08-28 10:17:42 +0300

    Adopt latest proxy framework (#14)
    * reverse_proxy
    
    * reverse_proxy
    
    * build
    
    * fix config
    
    * Fix self_host
    
    * Settings name adjustments
    
    * Update dependencies

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"github.com/Peripli/service-broker-proxy-cf/cf"
 	"fmt"
-	"github.com/Peripli/service-broker-proxy/pkg/sbproxy"
+
+	"github.com/Peripli/service-broker-proxy-cf/cf"
 	"github.com/Peripli/service-broker-proxy/pkg/middleware"
+	"github.com/Peripli/service-broker-proxy/pkg/sbproxy"
+	"github.com/Peripli/service-manager/pkg/util"
 	"github.com/spf13/pflag"
 )
 
 func main() {
+	ctx, cancel := util.HandleInterrupts()
+	defer cancel()
+
 	env := sbproxy.DefaultEnv(func(set *pflag.FlagSet) {
 		cf.CreatePFlagsForCFClient(set)
 	})
@@ -27,12 +32,10 @@ func main() {
 		panic(fmt.Errorf("error creating CF client: %s", err))
 	}
 
-	proxy, err := sbproxy.New(env, platformClient)
-	if err != nil {
-		panic(fmt.Errorf("error creating proxy: %s", err))
-	}
+	proxyBuilder := sbproxy.New(ctx, env, platformClient)
+	proxy := proxyBuilder.Build()
 
-	proxy.Server.Use(middleware.BasicAuth(platformConfig.Reg.User, platformConfig.Reg.Password))
+	proxy.Use(middleware.BasicAuth(platformConfig.Reg.User, platformConfig.Reg.Password))
 
 	proxy.Run()
 }

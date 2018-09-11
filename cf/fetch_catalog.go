09417756c611ce880ac0e692da98c9cd57e94b06
Dimitar Panayotov <dimitar.panayotov@sap.com>
2018-09-11 14:40:04 +0300

    adopt service-manager/pkg/log

--- cf/fetch_catalog.go
+++ cf/fetch_catalog.go
@@ -1,13 +1,16 @@
 package cf
 
-import "github.com/Peripli/service-broker-proxy/pkg/platform"
+import (
+	"context"
+	"github.com/Peripli/service-broker-proxy/pkg/platform"
+)
 
 var _ platform.CatalogFetcher = &PlatformClient{}
 
 // Fetch implements service-broker-proxy/pkg/cf/Fetcher.Fetch and provides logic for triggering refetching
 // of the broker's catalog
-func (pc PlatformClient) Fetch(broker *platform.ServiceBroker) error {
-	_, err := pc.UpdateBroker(&platform.UpdateServiceBrokerRequest{
+func (pc PlatformClient) Fetch(ctx context.Context, broker *platform.ServiceBroker) error {
+	_, err := pc.UpdateBroker(ctx, &platform.UpdateServiceBrokerRequest{
 		GUID:      broker.GUID,
 		Name:      broker.Name,
 		BrokerURL: broker.BrokerURL,
--- cf/service_broker.go
+++ cf/service_broker.go
@@ -2,13 +2,14 @@ package cf
 
 import
 (
-"github.com/Peripli/service-broker-proxy/pkg/platform"
+	"context"
+	"github.com/Peripli/service-broker-proxy/pkg/platform"
 "github.com/cloudfoundry-community/go-cfclient"
 )
 
 // GetBrokers implements service-broker-proxy/pkg/cf/Client.GetBrokers and provides logic for
 // obtaining the brokers that are already registered at the cf.
-func (pc PlatformClient) GetBrokers() ([]platform.ServiceBroker, error) {
+func (pc PlatformClient) GetBrokers(ctx context.Context) ([]platform.ServiceBroker, error) {
 	brokers, err := pc.Client.ListServiceBrokers()
 	if err != nil {
 		return nil, wrapCFError(err)
@@ -29,7 +30,7 @@ func (pc PlatformClient) GetBrokers() ([]platform.ServiceBroker, error) {
 
 // CreateBroker implements service-broker-proxy/pkg/cf/Client.CreateBroker and provides logic for
 // registering a new broker at the cf.
-func (pc PlatformClient) CreateBroker(r *platform.CreateServiceBrokerRequest) (*platform.ServiceBroker, error) {
+func (pc PlatformClient) CreateBroker(ctx context.Context, r *platform.CreateServiceBrokerRequest) (*platform.ServiceBroker, error) {
 
 	request := cfclient.CreateServiceBrokerRequest{
 		Username:  pc.reg.User,
@@ -54,7 +55,7 @@ func (pc PlatformClient) CreateBroker(r *platform.CreateServiceBrokerRequest) (*
 
 // DeleteBroker implements service-broker-proxy/pkg/cf/Client.DeleteBroker and provides logic for
 // registering a new broker at the cf.
-func (pc PlatformClient) DeleteBroker(r *platform.DeleteServiceBrokerRequest) error {
+func (pc PlatformClient) DeleteBroker(ctx context.Context, r *platform.DeleteServiceBrokerRequest) error {
 
 	if err := pc.Client.DeleteServiceBroker(r.GUID); err != nil {
 		return wrapCFError(err)
@@ -65,7 +66,7 @@ func (pc PlatformClient) DeleteBroker(r *platform.DeleteServiceBrokerRequest) er
 
 // UpdateBroker implements service-broker-proxy/pkg/cf/Client.UpdateBroker and provides logic for
 // updating a broker registration at the cf.
-func (pc PlatformClient) UpdateBroker(r *platform.UpdateServiceBrokerRequest) (*platform.ServiceBroker, error) {
+func (pc PlatformClient) UpdateBroker(ctx context.Context, r *platform.UpdateServiceBrokerRequest) (*platform.ServiceBroker, error) {
 
 	request := cfclient.UpdateServiceBrokerRequest{
 		Username:  pc.reg.User,

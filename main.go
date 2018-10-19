9c56c7af16c8337ecd11cc203584267fceabc8e8
Dimitar Panayotov <dpanayotov94@gmail.com>
2018-10-19 16:12:34 +0300

    Adopt new service-broker-proxy version (#19)
    * Adopt race condition fix
    
    * Adopt new sbproxy version

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/Peripli/service-broker-proxy-cf/cf"
-	"github.com/Peripli/service-broker-proxy/pkg/middleware"
 	"github.com/Peripli/service-broker-proxy/pkg/sbproxy"
-	"github.com/Peripli/service-manager/pkg/util"
 	"github.com/spf13/pflag"
 )
 
 func main() {
-	ctx, cancel := util.HandleInterrupts()
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	env := sbproxy.DefaultEnv(func(set *pflag.FlagSet) {
@@ -32,10 +31,8 @@ func main() {
 		panic(fmt.Errorf("error creating CF client: %s", err))
 	}
 
-	proxyBuilder := sbproxy.New(ctx, env, platformClient)
+	proxyBuilder := sbproxy.New(ctx, cancel, env, platformClient)
 	proxy := proxyBuilder.Build()
-
-	proxy.Use(middleware.BasicAuth(platformConfig.Reg.User, platformConfig.Reg.Password))
 
 	proxy.Run()
 }

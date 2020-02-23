7e8244a1041cbd66f7d9e1e4ffc6ef94b2416bf7
NickyMateev <bgdunkers@gmail.com>
2020-02-23 12:40:01 +0200

    Use newly generated broker platform credentials in requests to platform

--- cf/fetch_catalog.go
+++ cf/fetch_catalog.go
@@ -9,11 +9,7 @@ import (
 // Fetch implements service-broker-proxy/pkg/cf/Fetcher.Fetch and provides logic for triggering refetching
 // of the broker's catalog
 func (pc *PlatformClient) Fetch(ctx context.Context, r *platform.UpdateServiceBrokerRequest) error {
-	_, err := pc.UpdateBroker(ctx, &platform.UpdateServiceBrokerRequest{
-		GUID:      r.GUID,
-		Name:      r.Name,
-		BrokerURL: r.BrokerURL,
-	})
+	_, err := pc.UpdateBroker(ctx, r)
 
 	return err
 }

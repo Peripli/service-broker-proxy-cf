b2c7604246787144098622ca7610a5eec094d04e
Oleg Yudovich <oleg.tlv.il@gmail.com>
2022-08-15 18:58:53 +0300

    testhelper

--- cf/cfclient/client_test.go
+++ cf/cfclient/client_test.go
@@ -2,57 +2,19 @@ package cfclient_test
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/Peripli/service-broker-proxy-cf/cf/cfclient"
+	"github.com/Peripli/service-broker-proxy-cf/cf/internal"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
 	"golang.org/x/oauth2"
-	"net/http"
 )
 
 var (
 	ccServer *ghttp.Server
 	ctx      context.Context
 )
-
-func fakeCCServer(allowUnhandled bool) *ghttp.Server {
-	ccServer := ghttp.NewServer()
-	serverUrl := ccServer.URL()
-
-	v3RootCallResponse, err := json.Marshal(&cfclient.Endpoints{
-		Links: cfclient.Links{
-			AuthEndpoint: cfclient.EndpointUrl{
-				URL: serverUrl,
-			},
-			TokenEndpoint: cfclient.EndpointUrl{
-				URL: serverUrl,
-			},
-		},
-	})
-
-	Expect(err).ShouldNot(HaveOccurred())
-
-	ccServer.RouteToHandler(http.MethodGet, "/", func(res http.ResponseWriter, req *http.Request) {
-		res.Header().Set("Content-Type", "application/json")
-		res.WriteHeader(http.StatusOK)
-		res.Write(v3RootCallResponse)
-	})
-	ccServer.RouteToHandler(http.MethodPost, "/oauth/token", func(res http.ResponseWriter, req *http.Request) {
-		res.Header().Set("Content-Type", "application/json")
-		res.WriteHeader(http.StatusOK)
-		res.Write([]byte(`
-						{
-							"token_type":    "bearer",
-							"access_token":  "access",
-							"refresh_token": "refresh",
-							"expires_in":    "123456"
-						}`))
-	})
-	ccServer.AllowUnhandledRequests = allowUnhandled
-	return ccServer
-}
 
 var _ = Describe("Client", func() {
 	BeforeEach(func() {
@@ -80,7 +42,7 @@ var _ = Describe("Client", func() {
 
 	Describe("NewClient", func() {
 		It("should create valid client", func() {
-			server := fakeCCServer(false)
+			server := testhelper.FakeCCServer(false)
 			config := &cfclient.Config{
 				ApiAddress: server.URL() + "/",
 			}
@@ -98,7 +60,7 @@ var _ = Describe("Client", func() {
 		})
 
 		It("should create valid client with token", func() {
-			server := fakeCCServer(false)
+			server := testhelper.FakeCCServer(false)
 			serverUrl := server.URL()
 			config := &cfclient.Config{
 				ApiAddress: serverUrl,
